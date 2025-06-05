82570d86a436621beff60f951fa43a262b195ae9
subathran <2326jb49@kgcas.com>
2025-06-05 12:31:00 +0530

    new1.java

--- new1.java
+++ new1.java
@@ -77,3 +77,17 @@ class Main {
      }
     }
 }
+---------------------------------------------------------------------------------------------------------------------------------
+4)//do while
+class Main {
+    public static void main(String[]args)
+    {
+        int i=5;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<10);
+    }
+}
