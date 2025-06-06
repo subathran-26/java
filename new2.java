50f04573707b982681e012146d0d2e2c477b7fd7
subathran <2326jb49@kgcas.com>
2025-06-06 11:26:41 +0530

    new2.java

--- new2.java
+++ new2.java
@@ -11,3 +11,32 @@ class Main{
         System.out.println(sum);
     }
 }
+--------------------------------------------------------------------------------
+    2)
+import java.util.Scanner;
+    class main
+    {
+        public static void main(String args[])
+        {
+            Scanner in=new Scanner(System.in);
+             System.out.println("enter the size of the array");
+             int n=in.nextInt();
+             int[] arr=new int[n];
+              System.out.println("enter the value");
+             for(int i=0;i<n;i++)
+        {
+            arr[i] =in.nextInt();
+             if(arr[i]%2==0)
+            {
+              System.out.println("the value is even="+arr[i]);
+            }
+            else
+            {
+                System.out.println("the value is odd="+arr[i]);
+            }
+           
+        }
+        System.out.println("numbers of values are:"+n);
+     
+}
+}
