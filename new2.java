2d820dd4290d1f30ca65ecfcc9efe4408bfd0fa6
subathran <2326jb49@kgcas.com>
2025-06-09 09:12:41 +0530

    new2.java

--- new2.java
+++ new2.java
@@ -98,7 +98,7 @@ class Main {
 5)
 public class ReverseStringNoSubstring {
     public static void main(String[] args) {
-        String input = "Abhinav";
+        String input = "subathran";
         String reversed = "";
 
         for (int i = input.length() - 1; i >= 0; i--) {
